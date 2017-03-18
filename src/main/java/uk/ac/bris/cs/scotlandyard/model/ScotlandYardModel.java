ac9b2b274ca0a65f26abf148a2b76beaf36a9d12
thewalkingfridge <martin.dimi97@gmail.com>
2017-03-18 15:42:26 +0000

    restructured code

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -12,9 +12,6 @@ import static java.util.Objects.requireNonNull;
 import static uk.ac.bris.cs.scotlandyard.model.Colour.*;
 import static uk.ac.bris.cs.scotlandyard.model.Ticket.*;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -24,62 +21,90 @@ import uk.ac.bris.cs.gamekit.graph.Graph;
 import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 import uk.ac.bris.cs.gamekit.graph.Graph;
 
-// TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
 	
-	public PlayerConfiguration mrX, firstDetective;
 	public List<Boolean> rounds;
 	public Graph<Integer, Transport> graph;
-	public ArrayList<PlayerConfiguration> restOfTheDetectives = new ArrayList<>();
+	public List<PlayerConfiguration> players;
 	public HashMap<Colour, Integer> colours;
 	
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-
-		if(requireNonNull(rounds).isEmpty()) throw new IllegalArgumentException("Rounds is empty");
-		if(requireNonNull(graph).isEmpty()) throw new IllegalArgumentException("Graph is empty");
-
-		this.rounds = rounds;
-		this.graph = graph;
-
+		
+		players = new ArrayList<>();
 		colours = new HashMap<>();
+		colours.put(Black, 0);
 		colours.put(Blue, 0);
 		colours.put(Red, 0);
-		colours.put(Black, 0);
 		colours.put(Green, 0);
 		colours.put(Yellow, 0);
-
-		this.mrX = requireNonNull(mrX);
-		this.firstDetective = requireNonNull(firstDetective);
-		colours.put(firstDetective.colour, colours.get(firstDetective.colour) + 1);
-		for(PlayerConfiguration current : restOfTheDetectives) {
-
-			this.restOfTheDetectives.add(requireNonNull(current));
-			if (current.tickets.get(Double) != 0) throw new IllegalArgumentException("Any of restOfTheDetectives has double tickets.");
-			if (current.tickets.get(Secret) != 0) throw new IllegalArgumentException("Any of restOfTheDetecrives has secret tickets.");
-
-		}
-
-		if(!mrX.colour.isMrX()) throw new IllegalArgumentException("There is no MRX");
-		if(firstDetective.colour.isMrX()) throw new IllegalArgumentException("There is more than one MRX");
-		for(PlayerConfiguration current : this.restOfTheDetectives)
-			colours.put(current.colour, colours.get(current.colour) + 1);
-		if(colours.containsValue(2)) throw new IllegalArgumentException("This detective already exists.");
-
-		if(!(mrX.tickets.containsKey(Double) && mrX.tickets.containsKey(Secret) && mrX.tickets.containsKey(Taxi) && mrX.tickets.containsKey(Underground) && mrX.tickets.containsKey(Bus))) throw new IllegalArgumentException("MrX has no tickets");
-		if(!(firstDetective.tickets.containsKey(Double) && firstDetective.tickets.containsKey(Secret) && firstDetective.tickets.containsKey(Taxi) && firstDetective.tickets.containsKey(Underground) && firstDetective.tickets.containsKey(Bus))) throw new IllegalArgumentException("FirstDetective has no tickets");
-
-
-		if(firstDetective.tickets.get(Double) != 0) throw new IllegalArgumentException("First detective has double tickets.");
-		if(firstDetective.tickets.get(Secret) != 0) throw new IllegalArgumentException("First detective has secret tickets.");
-
-        if(firstDetective.location == mrX.location) throw new IllegalArgumentException("Location of mrX and firstDetective overlap.");//NAPRAVI GO ZA VSICHKI DETEKTIVI
-
-
+		colours.put(White, 0);
+		
+		this.rounds = requireNonNull(rounds);
+		this.graph = requireNonNull(graph);
+		players.add(requireNonNull(mrX));
+		players.add(requireNonNull(firstDetective));
+		for(PlayerConfiguration current : restOfTheDetectives)
+			players.add(requireNonNull(current));
+		
+		ValidMapCheck();
+		ValidPlayersCheck();	// Should it be a throw function -- sisi, tova e za men da pitam
+		ValidTicketSlotsCheck();
+		ValidDetectiveTicketsCheck();
 
 	}
 
+	
+	private void ValidMapCheck(){
+		if(requireNonNull(rounds).isEmpty()) throw new IllegalArgumentException("Rounds is empty");
+		if(requireNonNull(graph).isEmpty()) throw new IllegalArgumentException("Graph is empty");
+	}
+	
+	private void ValidPlayersCheck (){
+		if(!players.get(0).colour.isMrX()) throw new IllegalArgumentException("MrX is missing");
+		
+		for(PlayerConfiguration current : players){
+			Colour currentColour = current.colour;
+			colours.put(currentColour, colours.get(currentColour) + 1);
+			if(colours.get(currentColour) > 1) throw new IllegalArgumentException("Players duplicate");
+		}
+		ValidPositionsCheck();
+	}
+	
+	private void ValidPositionsCheck(){ // is this a good idea or it's not dry enough? -- pak iskam da pitam tam
+		Iterator<PlayerConfiguration> detectives = players.listIterator(1);
+		while(detectives.hasNext()){
+			PlayerConfiguration detective = detectives.next();
+			if(detective.location == players.get(0).location) 
+				throw new IllegalArgumentException("Detective has spawned with the same location as mrX");
+		}
+	}
+	
+	private void ValidTicketSlotsCheck(){
+		for(PlayerConfiguration current : players){
+			Map<Ticket, Integer> tickets = current.tickets;
+			if(!(tickets.containsKey(Bus)  	  	 &&
+				 tickets.containsKey(Taxi) 	  	 &&
+				 tickets.containsKey(Underground)&&
+				 tickets.containsKey(Double)     &&
+				 tickets.containsKey(Secret)))
+					throw new IllegalArgumentException("One of the players doesn't have all tickets!");	
+			
+		}
+	}
+	
+	
+	
+	private void ValidDetectiveTicketsCheck(){
+		Iterator<PlayerConfiguration> detectives = players.listIterator(1);
+		while(detectives.hasNext()){
+			PlayerConfiguration detective = detectives.next();
+			if(detective.tickets.get(Double) != 0) throw new IllegalArgumentException("Detective has double tickets");
+			if(detective.tickets.get(Secret) != 0) throw new IllegalArgumentException("Detective has secret tickets");
+		}
+	}
+	
 	@Override
 	public void registerSpectator(Spectator spectator) {
 		// TODO
